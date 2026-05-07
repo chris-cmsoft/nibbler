a8646df8e200ee8e6895681d065b6d5aa9dba33e
Chris Vermeulen <christiaan.vermeulen@thebrief.ai>
2026-05-07 15:25:51 +0200

    Add more animals

--- database/seeders/AnimalSeeder.php
+++ database/seeders/AnimalSeeder.php
@@ -12,14 +12,35 @@ class AnimalSeeder extends Seeder
      */
     public function run(): void
     {
-        Animal::updateOrCreate(
-            ['slug' => 'mouse'],
+        $animals = [
             [
+                'slug' => 'mouse',
                 'name' => 'Mouse',
-                'calories_per_day' => 300,
-                'attention_points' => 50,
+                'calories_per_day' => 30,
+                'attention_points' => 10,
                 'svg_path' => 'resources/animals/mouse.svg',
             ],
-        );
+            [
+                'slug' => 'elephpant',
+                'name' => 'ElePHPant',
+                'calories_per_day' => 70000,
+                'attention_points' => 50,
+                'svg_path' => 'resources/animals/elephpant.svg',
+            ],
+            [
+                'slug' => 'pug',
+                'name' => 'Pug',
+                'calories_per_day' => 600,
+                'attention_points' => 60000,
+                'svg_path' => 'resources/animals/pug.svg',
+            ],
+        ];
+
+        foreach ($animals as $animal) {
+            Animal::updateOrCreate(
+                ['slug' => $animal['slug']],
+                $animal,
+            );
+        }
     }
 }
--- tests/Feature/Seeders/AnimalSeederTest.php
+++ tests/Feature/Seeders/AnimalSeederTest.php
@@ -11,20 +11,35 @@ class AnimalSeederTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_animal_seeder_creates_the_mouse(): void
+    public function test_animal_seeder_creates_the_bundled_animals(): void
     {
         $this->seed(AnimalSeeder::class);
 
+        $this->assertDatabaseCount('animals', 3);
         $this->assertDatabaseHas('animals', [
             'slug' => 'mouse',
             'name' => 'Mouse',
-            'calories_per_day' => 300,
-            'attention_points' => 50,
+            'calories_per_day' => 30,
+            'attention_points' => 10,
             'svg_path' => 'resources/animals/mouse.svg',
+        ]);
+        $this->assertDatabaseHas('animals', [
+            'slug' => 'elephpant',
+            'name' => 'ElePHPant',
+            'calories_per_day' => 70000,
+            'attention_points' => 50,
+            'svg_path' => 'resources/animals/elephpant.svg',
+        ]);
+        $this->assertDatabaseHas('animals', [
+            'slug' => 'pug',
+            'name' => 'Pug',
+            'calories_per_day' => 600,
+            'attention_points' => 60000,
+            'svg_path' => 'resources/animals/pug.svg',
         ]);
     }
 
-    public function test_animal_seeder_updates_the_mouse_by_slug(): void
+    public function test_animal_seeder_updates_animals_by_slug(): void
     {
         Animal::factory()->create([
             'slug' => 'mouse',
@@ -33,16 +48,44 @@ class AnimalSeederTest extends TestCase
             'attention_points' => 5,
             'svg_path' => 'resources/animals/old-mouse.svg',
         ]);
+        Animal::factory()->create([
+            'slug' => 'elephpant',
+            'name' => 'Old Elephant',
+            'calories_per_day' => 10,
+            'attention_points' => 10,
+            'svg_path' => 'resources/animals/old-elephant.svg',
+        ]);
+        Animal::factory()->create([
+            'slug' => 'pug',
+            'name' => 'Old Pug',
+            'calories_per_day' => 15,
+            'attention_points' => 15,
+            'svg_path' => 'resources/animals/old-pug.svg',
+        ]);
 
         $this->seed(AnimalSeeder::class);
 
-        $this->assertDatabaseCount('animals', 1);
+        $this->assertDatabaseCount('animals', 3);
         $this->assertDatabaseHas('animals', [
             'slug' => 'mouse',
             'name' => 'Mouse',
-            'calories_per_day' => 300,
-            'attention_points' => 50,
+            'calories_per_day' => 30,
+            'attention_points' => 10,
             'svg_path' => 'resources/animals/mouse.svg',
+        ]);
+        $this->assertDatabaseHas('animals', [
+            'slug' => 'elephpant',
+            'name' => 'ElePHPant',
+            'calories_per_day' => 70000,
+            'attention_points' => 50,
+            'svg_path' => 'resources/animals/elephpant.svg',
+        ]);
+        $this->assertDatabaseHas('animals', [
+            'slug' => 'pug',
+            'name' => 'Pug',
+            'calories_per_day' => 600,
+            'attention_points' => 60000,
+            'svg_path' => 'resources/animals/pug.svg',
         ]);
     }
 }
